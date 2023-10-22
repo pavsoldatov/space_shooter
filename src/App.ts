4e5ebcd434b5204a62fb852b6e833ce970888078
pavsoldatov <snowschoe@gmail.com>
2023-10-23 01:35:54 +0300

    Decouple App logic into scene, wip: asset loading

--- src/App.ts
+++ src/App.ts
@@ -1,29 +1,12 @@
-import { Application, Assets } from "pixi.js";
+import { Application } from "pixi.js";
 
 import "./style.css";
-import { manifest } from "./assets";
-import { constants } from "./constants";
-import {
-  Background,
-  PlayerShip,
-  AsteroidGroup,
-  GameTimer,
-  CollisionDetector,
-  ProjectileGroup,
-  HitCounter,
-} from "./";
-
+import { AssetLoader, LevelOneScene, constants } from "./";
 const { APP_WIDTH, APP_HEIGHT } = constants.resolution;
 
 class App {
   private app: Application<HTMLCanvasElement>;
-  private background: Background | null = null;
-  private playerShip: PlayerShip | null = null;
-  private asteroidGroup: AsteroidGroup | null = null;
-  private projectiles: ProjectileGroup | null = null;
-  private gameTimer: GameTimer | null = null;
-  private collisionDetector: CollisionDetector | null = null;
-  private hitCounter: HitCounter | null = null;
+  private levelOneScene: LevelOneScene;
 
   constructor() {
     this.app = new Application({
@@ -31,40 +14,13 @@ class App {
       height: APP_HEIGHT,
       antialias: false,
     });
+    this.app.stage.sortableChildren = true;
     this.app.view.classList.add("game_view");
     document.body.appendChild(this.app.view);
 
-    this.initAssets().then(() => {
-      this.background = new Background(this.app);
-      this.hitCounter = new HitCounter(this.app);
-      this.asteroidGroup = new AsteroidGroup(this.app);
-      this.gameTimer = new GameTimer(this.app);
-      this.projectiles = new ProjectileGroup(this.app);
-      this.playerShip = new PlayerShip(this.app, this.projectiles);
-      this.collisionDetector = new CollisionDetector(
-        this.projectiles,
-        this.asteroidGroup,
-        this.hitCounter
-      );
-    });
-
-    this.app.ticker.add((delta) => this.update(delta));
-  }
-
-  private async initAssets() {
-    await Assets.init({ manifest });
-    await Assets.loadBundle("level-1");
-  }
-
-  private update(delta: number) {
-    this.background?.update(delta);
-    this.asteroidGroup?.update(delta);
-    this.playerShip?.update(delta);
-    this.gameTimer?.update(); // relies on Date.now() instead of delta;
-    this.collisionDetector?.checkCollisions(
-      this.playerShip?.getX()!,
-      this.playerShip?.getY()!
-    );
+    AssetLoader.getInstance().init();    
+    this.levelOneScene = new LevelOneScene(this.app); // will need to switch scenes
+    this.app.stage.addChild(this.levelOneScene);
   }
 }
 
