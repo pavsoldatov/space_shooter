b0bd4673110732748208a4257e973ac90289c597
pavsoldatov <snowschoe@gmail.com>
2023-10-24 18:54:33 +0300

    Add a mixin for sharing instances across scenes

---
+++ src/ShareableMixin.ts
@@ -0,0 +1,20 @@
+const InstanceStore = new Map<string, any>();
+
+export function ShareableMixin<T extends { new (...args: any[]): {} }>(BaseClass: T, className: string) {
+  return class extends BaseClass {
+    constructor(...args: any[]) {
+      super(...args);
+
+      console.log(InstanceStore, this.constructor.name)
+      if (InstanceStore.has(className)) {
+        console.error(`An instance of ${className} already exists. Call ${className}.getSharedInstance() to retrieve it.`);
+        return;
+      }
+      InstanceStore.set(className, this);
+    }
+
+    static getSharedInstance(): any {
+      return InstanceStore.get(className);
+    }
+  };
+}
